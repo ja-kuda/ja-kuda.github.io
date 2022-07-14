7040621867eb9826e0f6d7e000badf2e93ecf8a5
ja-kuda <jan-rehmeyer@hotmail.de>
2022-07-14 02:35:59 +0200

    dynamik und tags eingebaut

--- talents.js
+++ talents.js
@@ -1,22 +1,36 @@
 
 var allTalents;
+var checkBoxes;
 var activeTags = [];
 
 function loadFile(){
-    $.getJSON('talents.json', function( data ) {
-      console.log(data.talents[0]);
-      console.log(data.talents.length);
+    // build tag checkboxes
+    $.getJSON('tags.json', function( data ) {
+        checkBoxes = data;
+        createCheckboxes();
+    });
 
-      data.talents.forEach(element => {
-        console.log(element.name);
-        console.log(element.prerequisites);
-        console.log(element.charges);
-        console.log(element.costs);
-        console.log(element.effects);
-        console.log(element.tags);
-      });
+    // read all talents
+    $.getJSON('talents.json', function( data ) {
       allTalents = data;
       showAllTalents();
+    });
+}
+
+
+function createCheckboxes(){
+    checkBoxes.tags.forEach(element => {
+        const cb = document.createElement("input");
+        cb.setAttribute("type", "checkbox");
+        cb.setAttribute("onClick", "filter(this)")
+        cb.setAttribute("id", element);
+        const newlabel = document.createElement("Label");
+        newlabel.innerHTML = element;
+        const br = document.createElement("br");
+        const setter = document.getElementById("numberSetter");
+        setter.parentNode.insertBefore(cb, setter);
+        setter.parentNode.insertBefore(newlabel, setter);
+        setter.parentNode.insertBefore(br, setter);
     });
 }
 
@@ -25,20 +39,36 @@ function showAllTalents(){
     allTalents.talents.forEach(element => {
         const newDiv = document.createElement("div");
         newDiv.setAttribute("id", element.name);
-        const newContent = document.createTextNode(element.name + " - " + element.effects);
-        newDiv.appendChild(newContent);
+        newDiv.setAttribute("class", "talentBox");
+        const br = document.createElement("br");
+        const name = document.createTextNode("Name: " + element.name);
+        newDiv.appendChild(name);
+        newDiv.appendChild(br);
+        const prereq = document.createTextNode("Prerequisites: " + element.prerequisites);
+        newDiv.appendChild(prereq);
+        newDiv.appendChild(br.cloneNode());
+        const charges = document.createTextNode("Charges: " + element.charges);
+        newDiv.appendChild(charges);
+        newDiv.appendChild(br.cloneNode());
+        const costs = document.createTextNode("Costs: " + element.costs);
+        newDiv.appendChild(costs);
+        newDiv.appendChild(br.cloneNode());
+        const effects = document.createTextNode("Effects: " + element.effects);
+        newDiv.appendChild(effects);
         const currentDiv = document.getElementById("setter");
-        document.body.insertBefore(newDiv, currentDiv);
+        currentDiv.parentNode.insertBefore(newDiv, currentDiv);
     });
+    countTalents(allTalents.talents.length);
 }
 
 
-function resetfilters(){
-
+function countTalents(activeNumber){
+    document.getElementById("numberSetter").innerHTML = activeNumber + " / " + allTalents.talents.length;
 }
 
 
 function filter(checkbox){
+    var activeNumber = 0;
     if(checkbox.checked){
         activeTags.push(checkbox.id);
     }
@@ -66,9 +96,11 @@ function filter(checkbox){
         }
         if(hasTag){
             document.getElementById(element.name).style.display = 'block';
+            activeNumber++;
         }
         else{
             document.getElementById(element.name).style.display = 'none';
         }
     });
+    countTalents(activeNumber);
 }
